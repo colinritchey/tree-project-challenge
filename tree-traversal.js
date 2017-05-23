10a11367f805942d99aaf2fb958a6ea650dc3769
Colin Ritchey <colinritchey256@gmail.com>
2017-05-23 13:00:04 -0700

    started work on tree-traversl.js, added tree.html

--- tree-traversal.js
+++ tree-traversal.js
@@ -1,1 +1,26 @@
-let tree = {};
+// var $ = require("jquery");
+
+let tree = { body:{ children: [] } };
+
+function treeTraversal(element, parent){
+  let children = $(element).children();
+  console.log(children);
+
+  if(element === 'body'){
+    tree['body'].children = children;
+  } else {
+    debugger;
+    tree[parent][element] = element;
+    tree[parent][element].children = children;
+  }
+
+  debugger;
+  $.each(children, (idx, el) => {
+    if(el.children){
+      // debugger;
+      if(el.children.length > 0){
+        treeTraversal(el, element);
+      }
+    }
+  })
+}
